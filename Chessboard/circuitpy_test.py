db7f84fc81ed9765b2c570c235d747db55cdcb3a
JorgeFishkin <coletonkin@gmail.com>
2022-11-09 00:40:21 -0500

    CircuitPy Tests

---
+++ Chessboard/circuitpy_test.py
@@ -0,0 +1,29 @@
+import time
+import board
+import digitalio
+import busio
+
+from adafruit_mcp230xx.mcp23017 import MCP23017
+
+i2c = busio.I2C(board.SCL, board.SDA)
+
+mcp1 = MCP23017(i2c, address=0x20)
+#mcp2 = MCP23017(i2c, address=0x21)
+
+pin1_0 = mcp1.get_pin(0)
+#pin2_0 = mcp2.get_pin(0)
+
+#pin2_0.direction = digitalio.Direction.INPUT
+#pin2_0.pull = digitalio.Pull.UP
+
+
+pin1_0.value = False
+try:
+    while True:
+        pin1_0.value = True
+        time.sleep(0.5)
+        pin1_0.value = False
+finally:
+    pin1_0.value = False
+
+
