fdf71874ab48f3a56957cc13d06463f389f3dffb
JorgeFishkin <coletonkin@gmail.com>
2022-11-11 15:56:05 -0500

    circuitpy2

--- Chessboard/circuitpy_test2.py
+++ Chessboard/circuitpy_test2.py
@@ -8,25 +8,26 @@ from adafruit_mcp230xx.mcp23017 import MCP23017
 i2c = busio.I2C(board.SCL, board.SDA)
 
 mcp1 = MCP23017(i2c, address=0x20)
-mcp2 = MCP23017(i2c, address=0x21)
+#mcp2 = MCP23017(i2c, address=0x21)
 
 pin1_0 = mcp1.get_pin(0)
-pin2_0 = mcp2.get_pin(0)
+#pin2_0 = mcp2.get_pin(0)
 
-pin2_0.direction = digitalio.Direction.INPUT
-pin2_0.pull = digitalio.Pull.UP
+pin1_0.direction = digitalio.Direction.INPUT
+pin1_0.pull = digitalio.Pull.UP
 
 
-pin1_0.value = False
+
 try:
     while True:
-        if(pin2_0.value == False):
-            pin1_0.value = True
+        if(pin1_0.value == False):
+            #pin1_0.value = True
             print("Sensor pressed")
             time.sleep(0.05)
         else:
-            pin1_0.value = False
+            print("Sensor not pressed")
+            time.sleep(0.05)
 
         
 finally:
-    pin1_0.value = False
+    #pin1_0.value = False
