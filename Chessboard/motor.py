ce903cc6944f4418fcde614948b19133770a6809
Cole Tonkin <cct0005@mix.wvu.edu>
2022-12-07 11:20:07 -0500

    motor

--- Chessboard/motor.py
+++ Chessboard/motor.py
@@ -11,8 +11,4 @@ for i in range(815):
     kit.stepper2.onestep()
     time.sleep(0.008)
 
-print("Backward Motor Drive")
-for i in range(815):
-    kit.stepper1.onestep(direction = stepper.BACKWARD)
-    kit.stepper2.onestep(direction = stepper.BACKWARD)
-    time.sleep(0.008)
+#
