fa6b7ed285d5721b44dc2fb4d1b4f778ef39cb24
Cole Tonkin <cct0005@mix.wvu.edu>
2022-12-07 09:54:46 -0500

    motor

--- Chessboard/motor.py
+++ Chessboard/motor.py
@@ -4,8 +4,15 @@ import board
 from adafruit_motorkit import MotorKit
 kit = MotorKit()
 
-kit.stepper1.onestep()
 
+print("Forward Motor Drive")
 for i in range(815):
     kit.stepper1.onestep()
+    kit.stepper2.onestep()
+    time.sleep(0.008)
+
+print("Backward Motor Drive")
+for i in range(815):
+    kit.stepper1.onestep(direction = stepper.BACKWARD)
+    kit.stepper2.onestep(direction = stepper.BACKWARD)
     time.sleep(0.008)
