14fe87e30beafa832d4a173f87b9b68aa1e7f6dd
Cole Tonkin <cct0005@mix.wvu.edu>
2022-12-07 11:44:20 -0500

    mag

--- Chessboard/magnet.py
+++ Chessboard/magnet.py
@@ -1,10 +1,10 @@
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.OUT, initial=0)
+GPIO.setup(4, GPIO.OUT, initial=0)
 
 try:
     while True:
-        GPIO.output(18,1)
+        GPIO.output(4,1)
 
 except KeyboardInterrupt: 
     GPIO.cleanup
