b1f8c19a104b995fd854c8c476b283ad0edc6dd9
Cole Tonkin <cct0005@mix.wvu.edu>
2022-12-12 20:32:57 -0500

    motor

--- Chessboard/motor.py
+++ Chessboard/motor.py
@@ -12,24 +12,40 @@ DIAG_MOVE = 321
 current_pos = [0,0]
 
 def move_Xmotor(x):
+    travel = 0
+    curr_X = current_pos[0]
     x = int(x)
-    if(x < 0):
-        x = -x
-        for k in range(x*ONE_SQUARE):
+
+    travel = x - curr_X
+    
+    if(travel < 0):
+        travel = -travel
+        for k in range(travel*ONE_SQUARE):
             kit.stepper1.onestep(direction=stepper.BACKWARD)
     else:
-        for k in range(x*ONE_SQUARE):
+        for k in range(travel*ONE_SQUARE):
             kit.stepper1.onestep(direction=stepper.FORWARD)
 
+    current_pos[0] = x
+    print("New position: " + str(current_pos))
+
 def move_Ymotor(y):
+    travel = 0
+    curr_Y = current_pos[1]
     y = int(y)
-    if(y < 0):
-        y = -y
-        for k in range(y*ONE_SQUARE):
-            kit.stepper2.onestep(direction=stepper.BACKWARD)
+
+    travel = y - curr_Y
+    
+    if(travel < 0):
+        travel = -travel
+        for k in range(travel*ONE_SQUARE):
+            kit.stepper1.onestep(direction=stepper.BACKWARD)
     else:
-        for k in range(y*ONE_SQUARE):
-            kit.stepper2.onestep(direction=stepper.FORWARD)
+        for k in range(travel*ONE_SQUARE):
+            kit.stepper1.onestep(direction=stepper.FORWARD)
+
+    current_pos[1] = y
+    print("New position: " + str(current_pos))
 
 def move_Diag(d, xdir, ydir):
     d = int(d)
