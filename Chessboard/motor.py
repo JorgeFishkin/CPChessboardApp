50c6958abb87f89782b696703cc881d99ff6d900
Cole Tonkin <cct0005@mix.wvu.edu>
2022-12-12 20:37:27 -0500

    motor

--- Chessboard/motor.py
+++ Chessboard/motor.py
@@ -11,44 +11,62 @@ DIAG_MOVE = 321
 
 current_pos = [0,0]
 
+# Move X motor
 def move_Xmotor(x):
-    travel = 0
-    curr_X = current_pos[0]
-    x = int(x)
+    print("Current Position: " + str(current_pos))
+    if(int(x) >= 0):
+        travel = 0
+        curr_X = current_pos[0]
+        x = int(x)
 
-    travel = x - curr_X
-    
-    if(travel < 0):
-        travel = -travel
-        for k in range(travel*ONE_SQUARE):
-            kit.stepper1.onestep(direction=stepper.BACKWARD)
+        travel = x - curr_X
+        
+        if(travel < 0):
+            travel = -travel
+            for k in range(travel*ONE_SQUARE):
+                kit.stepper1.onestep(direction=stepper.BACKWARD)
+                time.sleep(0.08)
+        else:
+            for k in range(travel*ONE_SQUARE):
+                kit.stepper1.onestep(direction=stepper.FORWARD)
+                time.sleep(0.08)
+
+        current_pos[0] = x
+        print("New position: " + str(current_pos))
+        return 1
     else:
-        for k in range(travel*ONE_SQUARE):
-            kit.stepper1.onestep(direction=stepper.FORWARD)
+        print("Invalid Move")
+        return 0
 
-    current_pos[0] = x
-    print("New position: " + str(current_pos))
-
+# Move Y motor
 def move_Ymotor(y):
-    travel = 0
-    curr_Y = current_pos[1]
-    y = int(y)
+    print("Current Position: " + str(current_pos))
+    if(int(y) >= 0):
+        travel = 0
+        curr_Y = current_pos[1]
+        y = int(y)
 
-    travel = y - curr_Y
-    
-    if(travel < 0):
-        travel = -travel
-        for k in range(travel*ONE_SQUARE):
-            kit.stepper1.onestep(direction=stepper.BACKWARD)
+        travel = y - curr_Y
+        
+        if(travel < 0):
+            travel = -travel
+            for k in range(travel*ONE_SQUARE):
+                kit.stepper1.onestep(direction=stepper.BACKWARD)
+                time.sleep(0.08)
+        else:
+            for k in range(travel*ONE_SQUARE):
+                kit.stepper1.onestep(direction=stepper.FORWARD)
+                time.sleep(0.08)
+
+        current_pos[1] = y
+        print("New position: " + str(current_pos))
     else:
-        for k in range(travel*ONE_SQUARE):
-            kit.stepper1.onestep(direction=stepper.FORWARD)
-
-    current_pos[1] = y
-    print("New position: " + str(current_pos))
+        print("Invalid Move")
+        return 0
 
 def move_Diag(d, xdir, ydir):
     d = int(d)
     for k in range(d):
         kit.stepper1.onestep()
         kit.stepper2.onestep()
+        time.sleep(0.08)
