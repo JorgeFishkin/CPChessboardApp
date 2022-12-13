b785318ee75be5bf0fab951b7c83c9d2c2da414a
Cole Tonkin <cct0005@mix.wvu.edu>
2022-12-12 22:45:26 -0500

    bluetooth

--- Chessboard/pi_blue.py
+++ Chessboard/pi_blue.py
@@ -4,8 +4,8 @@ target_name ="DESKTOP-E3SIV59"
 target_address = None
 
 def bluetooth_poll():
-    nearby_devices = bluetooth.discover_devices()
     print("Polling nearby devices...")
+    nearby_devices = bluetooth.discover_devices()
     for baddr in nearby_devices:
         if target_name == bluetooth.lookup_name( baddr ):
             target_address = baddr
