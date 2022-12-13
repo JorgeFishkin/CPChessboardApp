25ec42be733c371ab0d70acc45b59431a3d3a08e
Cole Tonkin <cct0005@mix.wvu.edu>
2022-12-12 23:01:25 -0500

    motor speed test

--- Chessboard/motor.py
+++ Chessboard/motor.py
@@ -25,11 +25,11 @@ def move_Xmotor(x):
             travel = -travel
             for k in range(travel*ONE_SQUARE):
                 kit.stepper1.onestep(direction=stepper.BACKWARD)
-                time.sleep(0.008)
+                time.sleep(0.005)
         else:
             for k in range(travel*ONE_SQUARE):
                 kit.stepper1.onestep(direction=stepper.FORWARD)
-                time.sleep(0.008)
+                time.sleep(0.005)
 
         current_pos[0] = x
         print("New position: " + str(current_pos))
@@ -52,11 +52,11 @@ def move_Ymotor(y):
             travel = -travel
             for k in range(travel*ONE_SQUARE):
                 kit.stepper2.onestep(direction=stepper.BACKWARD)
-                time.sleep(0.008)
+                time.sleep(0.005)
         else:
             for k in range(travel*ONE_SQUARE):
                 kit.stepper2.onestep(direction=stepper.FORWARD)
-                time.sleep(0.008)
+                time.sleep(0.005)
 
         current_pos[1] = y
         print("New position: " + str(current_pos))
