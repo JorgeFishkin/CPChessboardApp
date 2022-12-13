f6acff7a749b5efe505a15f5e759ae8f07c129f4
Cole Tonkin <cct0005@mix.wvu.edu>
2022-12-12 20:09:42 -0500

    motor fix

--- Chessboard/motor.py
+++ Chessboard/motor.py
@@ -8,20 +8,22 @@ ONE_INCH = 0
 DIAG_MOVE = 0
 
 def move_Xmotor(x):
-    dir = stepper.FORWARD
     if(x < 0):
         x = -x
-        dir = stepper.BACKWARD
-    for k in range(x):
-        kit.stepper1.onestep(direction=dir)
+        for k in range(x):
+            kit.stepper1.onestep(direction=stepper.BACKWARD)
+    else:
+        for k in range(x):
+            kit.stepper1.onestep(direction=stepper.FORWARD)
 
 def move_Ymotor(y):
-    dir = stepper.FORWARD
     if(y < 0):
         y = -y
-        dir = stepper.BACKWARD
-    for k in range(y):
-        kit.stepper2.onestep()
+        for k in range(y):
+            kit.stepper2.onestep(direction=stepper.BACKWARD)
+    else:
+        for k in range(y):
+            kit.stepper2.onestep(direction=stepper.FORWARD)
 
 def move_Diag(d, xdir, ydir):
     
